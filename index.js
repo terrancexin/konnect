3a8b8090e815ffcb9dac42dfe83a38046b24b331
Terrance Xin <xin.terrance@gmail.com>
2018-05-14 01:39:50 -0400

    Add test socket connection

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const http = require('http').Server(app);
 const mongoose = require('mongoose');
 const io = require('socket.io')(http);
 const redis = require('redis');
+const router = require('./router');
 
 mongoose.connect('mongodb://localhost:27017/konnect');
 mongoose.set('debug', true);
@@ -13,6 +14,13 @@ mongoose.set('debug', true);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
