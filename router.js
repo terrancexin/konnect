1246965b427005b1b234d8519491b25fbe356c87
Terrance Xin <xin.terrance@gmail.com>
2018-05-14 01:50:10 -0400

    Test working socket connection

--- router.js
+++ router.js
@@ -1,7 +1,11 @@
 module.exports = app => {
   app.get('/', (req, res) => {
-    res.send({ message: 'http GET is working' });
+    res.sendFile(__dirname + '/index.html');
   });
+
+  // app.get('/', (req, res) => {
+  //   res.send({ message: 'http GET is working' });
+  // });
 
   app.post('/login', (req, res) => {
     const { username } = req.body;
