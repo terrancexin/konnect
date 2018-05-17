ca729498c3230426bf05e3c1315fb3c4e16088fa
Terrance Xin <xin.terrance@gmail.com>
2018-05-17 06:35:40 -0400

    Add user connected socket

--- server/services/socketManager.js
+++ server/services/socketManager.js
@@ -1,23 +1,56 @@
 const io = require('../index').io;
-const { USER_JOINED, USER_DISCONNECTED } = require('../../constants');
-const UserModel = require('../models/user');
+const {
+  USER_CONNECTED,
+  USER_DISCONNECTED,
+  TYPING,
+  FETCH_USERS,
+  FETCH_MESSAGES,
+  LOGIN_ERROR,
+  LOGOUT,
+  MESSAGE_RECIEVED,
+  MESSAGE_SENT,
+} = require('../../constants');
+const OpenUserModel = require('../models/openUser');
+
+const getTime = date => {
+  return `${date.getHours()}:${('0' + date.getMinutes()).slice(-2)}`;
+};
 
 module.exports = socket => {
+  socket.on(USER_CONNECTED, user => {
+    console.log('on?');
+    console.log(user);
+    io.emit(USER_CONNECTED, user);
+    
+    socket.on('disconnect', () => {
 
-  socket.on(USER_JOINED, username => {
-    UserModel.findOne({ username }, (err, user) => {
-      if (user) {
-        io.emit(USER_JOINED, `${username} exists already`);
-      } else {
-        io.emit(USER_JOINED, 'show')
-      }
-    })
+      io.emit(USER_DISCONNECTED, user);
+    });
+  })
+  
+  socket.on(MESSAGE_SENT, data => {
     
+    io.emit(MESSAGE_SENT, data);
+  });
+  
+  socket.on(FETCH_USERS, data => {
+    console.log('fetch users', data);
     
+    io.emit(USER_CONNECTED, data.newUser);
+  });
+  
+  socket.on(FETCH_MESSAGES, () => {
+
+    io.emit(FETCH_MESSAGES, messages);
   });
 
-  socket.on('disconnect', user => {
+  socket.on(LOGOUT, () => {
 
-    // io.emit(USER_DISCONNECTED, connectedUsers);
+    io.emit(USER_DISCONNECTED, connectedUsers);
+  });
+
+  socket.on(TYPING, user => {
+    
+    io.emit(TYPING, user);
   });
 };
