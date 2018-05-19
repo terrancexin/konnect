842fccb7157cc71faf4d3feba9ba1e5d224ebf72
Terrance Xin <xin.terrance@gmail.com>
2018-05-19 00:20:11 -0400

    Add assets

--- server/router.js
+++ server/router.js
@@ -8,16 +8,18 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = app => {
-  app.get('/', requireAuth, (req, res) => {
-    // res.sendFile(__dirname + '/view/index.html');
-    res.send({ message: 'secret token' });
-  });
+  // app.get('/', requireAuth, (req, res) => {
+  //   // res.sendFile(__dirname + '/view/index.html');
+  //   res.send({ message: 'secret token' });
+  // });
 
-  app.get('/users', OpenUsersController.fetchAll);
+  app.post('/send', MessagesController.send);
   app.get('/messages', MessagesController.fetch);
+  
+  app.get('/users', OpenUsersController.fetchAll);
+  app.post('/openchat', OpenUsersController.login);
+  app.post('/removeuser', OpenUsersController.removeUser);
+  
   app.post('/login', requireSignin, UsersController.login);
   app.post('/signup', UsersController.signup);
-  app.post('/openchat', OpenUsersController.login);
-  app.post('/send', MessagesController.send);
-  app.post('/removeuser', OpenUsersController.removeUser);
 };
