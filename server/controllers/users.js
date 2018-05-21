ad52a95d4008f58e9d58e045589cf9153edac606
Terrance Xin <xin.terrance@gmail.com>
2018-05-21 16:17:35 -0400

    Add remove book mark route

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,49 +2,65 @@ const jwt = require('jwt-simple');
 const MessageModel = require('../models/message');
 const UserModel = require('../models/user');
 
-const fetchAll = (req, res, next) => {
-  UserModel.find({})
-    .exec((err, users) => {
-      if (err) return next(err);
+// Would extract `mySecretJwtKey` into a file and not push the file into github
+// declared here for demo purposes
+const mySecretJwtKey = 'my secret';
 
-      res.json(users);
-    });
+const tokenForUser = (user) => {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, mySecretJwtKey);
+};
+
+const fetchAll = (req, res, next) => {
+  UserModel.find({}).exec((err, users) => {
+    if (err) return next(err);
+
+    res.json(users);
+  });
 };
 
 const login = (req, res) => {
   const { username, password } = req.body;
-  
-  UserModel.update({ username }, {onlineStatus: true}, err => {
-    if (err) {
-      return console.log(`updating user's book mark failed: ${err}`);
+
+  UserModel.update({ username }, { onlineStatus: true }, (updateUserError) => {
+    if (updateUserError) {
+      return console.log(`updating user's online status failed: ${updateUserError}`);
     }
 
-    UserModel.findOne({ username}, (err, updatedUser) => {
+    UserModel.findOne({ username }, (findUserError, updatedUser) => {
+      if (findUserError) {
+        return console.log(`finding a user from login failed: ${findUserError}`);
+      }
+
       if (updatedUser.bookMark) {
-        MessageModel.find({}, (err, messages) => {
-          if (err) {
-            return  console.log(`fetching all msgs in login failed: ${err}`);
+        MessageModel.find({}, (messageModelError, messages) => {
+          if (messageModelError) {
+            return console.log(`fetching all msgs in login failed: ${messageModelError}`);
           }
-          
+
           let tracker = messages.length - 1;
           const missedMsg = [];
-          const { bookMark, username } = updatedUser;
-          
+          const { bookMark } = updatedUser;
+
           if (tracker > 0) {
-            while (messages[tracker]._id != bookMark && messages[tracker].username != username) {
+            while (
+              tracker >= 0 &&
+              messages[tracker]._id !== bookMark &&
+              messages[tracker].username !== username
+            ) {
               missedMsg.unshift(messages[tracker]);
               tracker--;
             }
-            res.send({ 
+            res.send({
               token: tokenForUser(updatedUser),
               newUser: updatedUser,
-              missedMsg
+              missedMsg,
             });
           } else {
-            res.send({ 
+            res.send({
               token: tokenForUser(updatedUser),
               newUser: updatedUser,
-              missedMsg
+              missedMsg,
             });
           }
         });
@@ -52,12 +68,12 @@ const login = (req, res) => {
         res.send({
           token: tokenForUser({ username, password }),
           newUser: { username, password },
-          missedMsg: []
-        })
+          missedMsg: [],
+        });
       }
-    })
+    });
   });
-}
+};
 
 const signup = (req, res, next) => {
   const { username, password, passwordConfirmation } = req.body;
@@ -65,65 +81,82 @@ const signup = (req, res, next) => {
   if (!username) {
     return res.send({ error: 'hey, enter something!' });
   }
-  
+
   if (username.length < 3) {
     return res.send({ error: '3 characters minimum' });
   }
 
-  if (username.length > 15 ) {
+  if (username.length > 15) {
     return res.send({ error: '15 characters max' });
   }
-  if (password.length > 15 ) {
+  if (password.length > 15) {
     return res.send({ error: 'password has exceeded the character limit' });
   }
-  if (passwordConfirmation.length > 15 ) {
+  if (passwordConfirmation.length > 15) {
     return res.send({ error: 'password has exceeded the character limit' });
   }
-  
+
   if (!password) {
-    return res.send({ error: 'password can\'t be blank' });
+    return res.send({ error: "password can't be blank" });
   }
-  
+
   if (!passwordConfirmation) {
     return res.send({ error: 'please confirm your password' });
   }
-  
-  if (password != passwordConfirmation) {
+
+  if (password !== passwordConfirmation) {
     return res.send({ error: 'passwords do not match' });
   }
-  
+
   UserModel.findOne({ username }, (err, user) => {
-    if (err) { return next(err); }
-    
-    if (user) { return res.send({ error: `${username} is already taken!` }); }
-    
+    if (err) {
+      return next(err);
+    }
+
+    if (user) {
+      return res.send({ error: `${username} is already taken!` });
+    }
+
     const newUser = new UserModel({
       username,
       password,
       bookMark: '',
-      onlineStatus: true
+      onlineStatus: true,
     });
-    
-    newUser.save(err => {
-      if (err) { return next(err); }
+
+    newUser.save((saveError) => {
+      if (saveError) {
+        return next(saveError);
+      }
     });
-    
+
     res.json({
       token: tokenForUser(newUser),
       missedMsg: [],
       newUser,
     });
   });
-}
+};
 
-const tokenForUser = user => {
-  const timestamp = new Date().getTime();
-  return jwt.encode({ sub: user.id, iat: timestamp }, 'my secret jwt');
-}
+const removeBookMark = (req, res) => {
+  const { username } = req.body;
+
+  if (!username) {
+    return res.send({ error: 'username not found, cannot remove bookmark' });
+  }
+
+  UserModel.update({ username }, { bookMark: null }, (updateUserError) => {
+    if (updateUserError) {
+      return console.log(`updating user's online status failed: ${updateUserError}`);
+    }
+
+    return res.send({ success: 'bookmark successfully removed' });
+  });
+};
 
 module.exports = {
   fetchAll,
   login,
-  signup
+  removeBookMark,
+  signup,
 };
-
--- server/router.js
+++ server/router.js
@@ -5,11 +5,12 @@ const passport = require('passport');
 
 const requireSignin = passport.authenticate('local', { session: false });
 
-module.exports = app => {
+module.exports = (app) => {
+  app.post('/bookmark', UsersController.removeBookMark);
   app.post('/login', requireSignin, UsersController.login);
   app.post('/signup', UsersController.signup);
   app.get('/users', UsersController.fetchAll);
-  
+
   app.get('/messages', MessagesController.fetch);
   app.post('/send', MessagesController.send);
 };
