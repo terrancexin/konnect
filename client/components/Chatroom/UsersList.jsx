b826895e4dab1540b5f3c9d11b8a86e148198728
Terrance Xin <xin.terrance@gmail.com>
2018-05-22 23:53:44 -0400

    Remove duplicated user list div

--- client/components/Chatroom/UsersList.jsx
+++ client/components/Chatroom/UsersList.jsx
@@ -3,23 +3,21 @@ import PropTypes from 'prop-types';
 
 const UsersList = ({ users }) => (
   <div className="users-list">
-    <div className="users-list">
-      {users.map((user) => {
-        const { _id, onlineStatus, username } = user;
-        const isOnline = onlineStatus ? 'active' : 'inactive';
+    {users.map((user) => {
+      const { _id, onlineStatus, username } = user;
+      const isOnline = onlineStatus ? 'active' : 'inactive';
 
-        return (
-          <div key={_id} className="each-user">
-            <img
-              className={`online-${isOnline}`}
-              src={`${ROOT_URL}/images/online.png`}
-              alt="online"
-            />
-            <div className={`each-user-name-${isOnline}`}>{username}</div>
-          </div>
-        );
-      })}
-    </div>
+      return (
+        <div key={_id} className="each-user">
+          <img
+            className={`online-${isOnline}`}
+            src={`${ROOT_URL}/images/online.png`}
+            alt="online"
+          />
+          <div className={`each-user-name-${isOnline}`}>{username}</div>
+        </div>
+      );
+    })}
   </div>
 );
 
