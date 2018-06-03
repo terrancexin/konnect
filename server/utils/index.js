ba8365d56ecae32c475c1eb23496dcf734516b49
Terrance Xin <xin.terrance@gmail.com>
2018-06-02 22:57:36 -0400

    Add disconnect socket logic

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,3 +1,37 @@
+const MessageModel = require('../models/message');
+const UserModel = require('../models/user');
+const { USER_DISCONNECTED } = require('../../constants');
+
+const disconnectUser = (socket, user) => {
+  const { username } = user;
+
+  MessageModel.findOne({}, {}, { sort: { _id: -1 } })
+    .then((message) => {
+      const { id } = message;
+
+      return UserModel.update(
+        { username },
+        { bookMark: message ? id : '', onlineStatus: false },
+      );
+    })
+    .then(() => UserModel.find({}))
+    .then((users) => {
+      const updatedUsers = users.map((_user) => {
+        const { id, onlineStatus } = _user;
+
+        return ({ id, username: _user.username, onlineStatus });
+      });
+
+      socket.broadcast.emit(USER_DISCONNECTED, {
+        users: updatedUsers,
+        notice: `Bye ${username}! Come back soon!🥂`,
+      });
+    })
+    .catch((err) => {
+      console.log(`disconnectUser failed: ${err}`);
+    });
+};
+
 const validateInputInfo = (username, password, passwordConfirmation) => {
   if (!username) {
     return 'hey, enter something!';
@@ -25,5 +59,6 @@ const validateInputInfo = (username, password, passwordConfirmation) => {
 };
 
 module.exports = {
+  disconnectUser,
   validateInputInfo,
 };
