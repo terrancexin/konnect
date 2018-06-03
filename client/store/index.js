0fc7bea2ada43e69fedb3a882af2bd4b0dbd0ed6
Terrance Xin <xin.terrance@gmail.com>
2018-06-03 00:24:00 -0400

    Fix lint error

--- client/store/index.js
+++ client/store/index.js
@@ -1,13 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-// import logger from 'redux-logger';
-
 import rootReducer from '../reducers';
+// import logger from 'redux-logger';
 
 // const middlewares = [thunk, logger];
 const middlewares = [thunk];
 
-const configureStore = (preloadedState) => createStore(
+const configureStore = preloadedState => createStore(
   rootReducer,
   preloadedState,
   applyMiddleware(...middlewares),
