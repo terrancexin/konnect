37c544ac347d2f778177afb130bc3e5545ab82a5
Terrance Xin <xin.terrance@gmail.com>
2018-06-06 10:25:33 -0400

    Change toggle emoji to use redux state

--- client/components/Chatroom/Emoji.jsx
+++ client/components/Chatroom/Emoji.jsx
@@ -1,38 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Picker } from 'emoji-mart';
+import { connect } from 'react-redux';
 
-const EmojiBtn = ({ addEmoji, toggleEmojiPicker, handleEmojiPicker }) => (
-  <div className="emoji">
-    <button onClick={handleEmojiPicker} className="emoji-btn">
-      <i className="far fa-smile" />
-    </button>
-    {toggleEmojiPicker && (
-      <Picker
-        emojiSize={20}
-        onSelect={addEmoji}
-        sheetSize={20}
-        showPreview={false}
-        showSkinTones={false}
-        perLine={5}
-        include={['recent', 'people', 'foods', 'nature', 'symbols']}
-        style={{
-          position: 'absolute',
-          bottom: '20px',
-          right: '20px',
-          height: '300px',
-          overflow: 'scroll',
-        }}
-      />
-    )}
-  </div>
-);
+import { handleToggleEmoji, handleToggleGiphy } from '../../actions';
 
-EmojiBtn.propTypes = {
+class Emoji extends Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    if (this.props.toggleEmoji) {
+      this.props.handleToggleEmoji(false);
+    } else {
+      this.props.handleToggleEmoji(true);
+      this.props.handleToggleGiphy(false);
+    }
+  }
+
+  render() {
+    const { addEmoji, toggleEmoji } = this.props;
+
+    return (
+      <div className="emoji">
+        <button onClick={this.handleClick} className="emoji-btn">
+          <i className="far fa-smile" />
+        </button>
+        {toggleEmoji && (
+          <Picker
+            emojiSize={20}
+            onSelect={addEmoji}
+            sheetSize={20}
+            showPreview={false}
+            showSkinTones={false}
+            perLine={5}
+            include={['recent', 'people', 'foods', 'nature', 'symbols']}
+            style={{
+              position: 'absolute',
+              bottom: '20px',
+              right: '20px',
+              height: '300px',
+              overflow: 'scroll',
+            }}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  toggleEmoji: state.toggleEmoji,
+});
+
+Emoji.propTypes = {
   addEmoji: PropTypes.func.isRequired,
-  toggleEmojiPicker: PropTypes.bool.isRequired,
-  handleEmojiPicker: PropTypes.func.isRequired,
+  toggleEmoji: PropTypes.bool.isRequired,
+  handleToggleEmoji: PropTypes.func.isRequired,
+  handleToggleGiphy: PropTypes.func.isRequired,
 };
 
-export default EmojiBtn;
+export default connect(mapStateToProps, {
+  handleToggleEmoji,
+  handleToggleGiphy,
+})(Emoji);
 
