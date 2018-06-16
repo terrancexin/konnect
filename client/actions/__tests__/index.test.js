eb232bf027c02edba7287fd18088fb923f6ceaf4
Terrance Xin <xin.terrance@gmail.com>
2018-06-16 12:30:03 -0400

    Set up action tests

---
+++ client/actions/__tests__/index.test.js
@@ -0,0 +1,52 @@
+import {
+  handleToggleEmoji,
+  setImgSrc,
+  setFileName,
+} from '../index';
+
+import {
+  TOGGLE_EMOJI,
+  SET_IMAGE_SRC,
+  SET_FILE_NAME,
+} from '../../../constants';
+
+describe('Emoji actions', () => {
+  it('handleToggleEmoji has the correct type', () => {
+    const action = handleToggleEmoji();
+
+    expect(action.type).toEqual(TOGGLE_EMOJI);
+  });
+
+  it('handleToggleEmoji has the correct payload', () => {
+    const action = handleToggleEmoji(true);
+
+    expect(action.payload).toEqual(true);
+  });
+});
+
+describe('Image upload actions', () => {
+  it('setImgSrc has the correct type', () => {
+    const action = setImgSrc();
+
+    expect(action.type).toEqual(SET_IMAGE_SRC);
+  });
+
+  it('setImgSrc has the correct payload', () => {
+    const action = setImgSrc('base64');
+
+    expect(action.payload).toEqual('base64');
+  });
+
+  it('setFileName has the correct type', () => {
+    const action = setFileName();
+
+    expect(action.type).toEqual(SET_FILE_NAME);
+  });
+
+  it('setFileName has the correct payload', () => {
+    const action = setFileName('name');
+
+    expect(action.payload).toEqual('name');
+  });
+});
+
