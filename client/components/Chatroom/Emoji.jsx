f0f1ae1dc0b6af6ef2aee06823a6aac622c08cd0
Terrance Xin <xin.terrance@gmail.com>
2018-06-19 18:07:12 -0400

    Add export class for tests

--- client/components/Chatroom/Emoji.jsx
+++ client/components/Chatroom/Emoji.jsx
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 
 import { handleToggleEmoji, handleToggleGiphy } from '../../actions';
 
-class Emoji extends Component {
+export class Emoji extends Component {
   constructor(props) {
     super(props);
 
