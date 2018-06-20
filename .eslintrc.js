a98e08b6bbd5940122bd993f7dfca5caa5ce0b8d
Terrance Xin <xin.terrance@gmail.com>
2018-06-19 21:24:47 -0400

    Update jest global variable

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,34 @@
 module.exports = { 
-  "extends": "airbnb",
-  "env": {
-    "node": true,
-    "es6": true
+  'extends': 'airbnb',
+  'env': {
+    'node': true,
+    'es6': true
   },
-  "rules": {
-    "react/forbid-prop-types": 0,
-    "camelcase":"off",
-    "object-curly-newline":"off",
-    "consistent-return": "off",
-    "no-param-reassign": "off",
-    "no-restricted-globals": "off",
-    "no-continue": "off",
-    "no-await-in-loop": "off",
-    "class-methods-use-this": "off",
-    "no-console": "off",
-    "func-names": "off",
-    "no-plusplus": "off"
+  'rules': {
+    'react/forbid-prop-types': 0,
+    'camelcase':'off',
+    'object-curly-newline':'off',
+    'consistent-return': 'off',
+    'no-param-reassign': 'off',
+    'no-restricted-globals': 'off',
+    'no-continue': 'off',
+    'no-await-in-loop': 'off',
+    'class-methods-use-this': 'off',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-plusplus': 'off'
   },
-  "globals": {
-    "it": true,
-    "expect": true,
-    "describe": true,
-    "beforeEach": true,
-    "afterEach": true,
-    "localStorage": true,
-    "FileReader": true,
-    "document": true,
-    "ROOT_URL": true,
-    "window": true
+  'globals': {
+    'it': true,
+    'expect': true,
+    'describe': true,
+    'beforeEach': true,
+    'afterEach': true,
+    'localStorage': true,
+    'FileReader': true,
+    'document': true,
+    'ROOT_URL': true,
+    'window': true,
+    'jest': true,
   }
 }
