70e9ed829ffaf8a270b617c2a59145ee40fa4c6d
Terrance Xin <xin.terrance@gmail.com>
2018-06-21 00:09:10 -0400

    Add message submit component

---
+++ client/components/Chatroom/MessageSubmit.jsx
@@ -0,0 +1,159 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import {
+  handleToggleEmoji,
+  handleToggleGiphy,
+  isTyping,
+  sendMessage,
+  setImgSrc,
+  setFileName,
+} from '../../actions';
+
+import EmojiPicker from './Emoji';
+import Giphy from './Giphy';
+import ImageUpload from './ImageUpload';
+import Typing from './Typing';
+
+class MessageSubmit extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      text: '',
+      textCount: 0,
+    };
+
+    this.addEmoji = this.addEmoji.bind(this);
+    this.closeEmojiGiphy = this.closeEmojiGiphy.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTypingTime = null;
+  }
+
+  addEmoji(emoji) {
+    this.setState({ text: `${this.state.text}${emoji.native}` });
+  }
+
+  closeEmojiGiphy() {
+    this.props.handleToggleGiphy(false);
+    this.props.handleToggleEmoji(false);
+  }
+
+  handleChange(e) {
+    const { username } = this.props.user;
+    const text = e.target.value || '';
+
+    clearTimeout(this.handleTypingTime);
+    this.props.isTyping(username, true);
+    this.handleTypingTime = setTimeout(() => {
+      this.props.isTyping(username, false);
+    }, 2000);
+
+    this.setState({ text, textCount: text.length });
+  }
+
+  handleKeyPress(e) {
+    const { value } = e.target;
+
+    if (e.key === 'Enter' && value) {
+      this.handleSubmit(e);
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({ text: '', textCount: 0, date: new Date() });
+    const { text, date } = this.state;
+    const { imgSrc, user: { username, avatar } } = this.props;
+
+    this.props.handleToggleGiphy(false);
+    this.props.handleToggleEmoji(false);
+    this.props.sendMessage({
+      userAvatar: avatar,
+      username,
+      text,
+      date,
+      imageMsg: imgSrc,
+    });
+
+    this.props.setImgSrc('');
+    this.props.setFileName('');
+
+    // clears file input event listener
+    document.getElementById('imageUploadInput').value = '';
+  }
+
+  render() {
+    const { text, textCount } = this.state;
+    const {
+      typing,
+      typingUsers,
+      verbs,
+      imgSrc,
+    } = this.props;
+
+    return (
+      <form onSubmit={this.handleSubmit} className="message-form">
+        <Typing typing={typing} typingUsers={typingUsers} verbs={verbs} />
+        <EmojiPicker addEmoji={this.addEmoji} />
+        <Giphy />
+        <ImageUpload />
+        <div className="message-input-box">
+          <input
+            autoComplete="off"
+            id="message"
+            maxLength="500"
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+            onClick={this.closeEmojiGiphy}
+            placeholder="enter your message"
+            type="text"
+            value={text}
+          />
+          <span className="character-count">{`${textCount}/500`}</span>
+          <button
+            className="send"
+            disabled={text.length < 1 && !imgSrc}
+            onClick={this.handleSubmit}
+          >
+            Send
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  imgSrc: state.imgSrc,
+  typing: state.typing,
+  typingUsers: state.typingUsers,
+  user: state.user,
+  verbs: state.verbs,
+});
+
+MessageSubmit.propTypes = {
+  imgSrc: PropTypes.string.isRequired,
+  typing: PropTypes.bool.isRequired,
+  typingUsers: PropTypes.array.isRequired,
+  user: PropTypes.object.isRequired,
+  verbs: PropTypes.string.isRequired,
+  handleToggleEmoji: PropTypes.func.isRequired,
+  handleToggleGiphy: PropTypes.func.isRequired,
+  isTyping: PropTypes.func.isRequired,
+  sendMessage: PropTypes.func.isRequired,
+  setImgSrc: PropTypes.func.isRequired,
+  setFileName: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, {
+  handleToggleEmoji,
+  handleToggleGiphy,
+  isTyping,
+  sendMessage,
+  setImgSrc,
+  setFileName,
+})(MessageSubmit);
