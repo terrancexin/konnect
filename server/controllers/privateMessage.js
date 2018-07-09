ba3e8d2bcaedffa88e30d437113ced46e05ae55a
Terrance Xin <xin.terrance@gmail.com>
2018-07-08 23:55:33 -0400

    Add encryption before saving private msg

--- server/controllers/privateMessage.js
+++ server/controllers/privateMessage.js
@@ -1,4 +1,8 @@
+const SimpleCrypto = require('simple-crypto-js').default;
 const PrivateMessageModel = require('../models/privateMessage');
+
+const secretCryptoKey = process.env.SECRET_CRYPTO_KEY || 'private';
+const simpleCrypto = new SimpleCrypto(secretCryptoKey);
 
 const getPrivateMessages = (req, res, next) => {
   PrivateMessageModel.find({}).sort({ date: 'asc' }).exec((err, messages) => {
@@ -10,6 +14,7 @@ const getPrivateMessages = (req, res, next) => {
 
 const sendPrivateMessage = (req, res, next) => {
   const { userAvatar, username, text, date, imageMsg } = req.body;
+  const encryptedText = simpleCrypto.encrypt(text);
 
   if (!username || !userAvatar || !date) {
     return res.send({ error: 'missing params in request' });
@@ -17,7 +22,7 @@ const sendPrivateMessage = (req, res, next) => {
 
   const message = new PrivateMessageModel({
     date,
-    text,
+    text: encryptedText,
     userAvatar,
     username,
     imageMsg,
