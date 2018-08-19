dd95547914f9a4ff5392345ba91ee7b64e4476a8
Terrance Xin <xin.terrance@gmail.com>
2018-08-19 17:49:00 -0400

    Add send message props

--- client/components/Chatroom/Mic.jsx
+++ client/components/Chatroom/Mic.jsx
@@ -2,29 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import Voice from '../Voice';
 import { handleToggleMic } from '../../actions/mic';
+import { sendMessage } from '../../actions/message';
 
 class Mic extends Component {
   constructor(props) {
     super(props);
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleCloseTranscript = this.handleCloseTranscript.bind(this);
   }
 
   handleClick(e) {
     e.preventDefault();
-    const { toggleMic, handleToggleMic } = this.props;
+    const { toggleMic } = this.props;
 
-    handleToggleMic(!toggleMic);
+    this.props.handleToggleMic(!toggleMic);
+  }
+
+  handleCloseTranscript() {
+    this.props.handleToggleMic(false);
   }
 
   render() {
-    const { toggleMic } = this.props;
-    let Voice;
-
-    if (toggleMic) {
-      Voice = require('../Voice');
-    }
+    const { toggleMic, user } = this.props;
 
     return (
       <div className="mic">
@@ -36,7 +38,11 @@ class Mic extends Component {
             <button onClick={this.handleCloseTranscript} className="mic__x">
               <i className="fas fa-times" />
             </button>
-            <Voice.default />
+            <Voice
+              user={user}
+              sendMessage={this.props.sendMessage}
+              handleToggleMic={this.props.handleToggleMic}
+            />
           </div>
         )}
       </div>
@@ -46,14 +52,17 @@ class Mic extends Component {
 
 const mapStateToProps = state => ({
   toggleMic: state.toggleMic,
+  user: state.user,
 });
 
 Mic.propTypes = {
   toggleMic: PropTypes.bool.isRequired,
   handleToggleMic: PropTypes.func.isRequired,
+  sendMessage: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
 };
 
 export default connect(
   mapStateToProps,
-  { handleToggleMic },
+  { handleToggleMic, sendMessage },
 )(Mic);
